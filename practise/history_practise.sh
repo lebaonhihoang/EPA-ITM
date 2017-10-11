fe604e58d8840775de8b235bb98d37608629658f
epa <epa@localhost.localdomain>
2017-10-11 20:53:24 +0100

    This was a practise for the ca1 coming up

---
+++ practise/history_practise.sh
@@ -0,0 +1,97 @@
+PWD
+   13  pwd
+   14  ps -ef
+   15  pwd | more
+   16  ls -l | more
+   17  la -l
+   18  ls -l
+   19  pwd | more
+   20  ps -ef | more
+   21  wc
+   22  wc?
+   23  wc ?
+   24  nano lab3
+   25  ./ lab3
+   26  ls
+   27  chmod +x lab3.sh
+   28  nano lab3
+   29  rm lab3
+   30  ls]
+   31  ls
+   32  nano lab3.sh
+   33  chmod +x lab3.sh
+   34  ./ lab3.sh
+   35  ls
+   36  ./lab3.sh
+   37  nano lab3.sh
+   38  ./lab3.sh
+   39  nano lab3.sh
+   40  ls -R
+   41  cd ..
+   42  ls -R
+   43  ls -l
+   44  ls -A
+   45  ls -a
+   46  ll
+   47  cd practise]
+   48  cd practise
+   49  nano lab3.sh
+   50  ./lab3.sh
+   51  cd .
+   52  cd week3
+   53  cd ..
+   54  cd week3
+   55  ls -l
+   56  nano hello.sh
+   57  cd..
+   58  cd ..
+   59  cd week4
+   60  ls
+   61  nano cpu_count.sh
+   62  cd ..
+   63  cd practise
+   64  ls
+   65  nano lab3.sh
+   66  cd ..
+   67  cd week3
+   68  cd ..
+   69  cd week4
+   70  nano cpu_count.sh 
+   71  cd ..
+   72  cd practisew
+   73  cd practise
+   74  nano lab3.sh
+   75  ./lab3.sh
+   76  nano lab3.sh
+   77  ./lab3.sh
+   78  nano lab3.sh
+   79  ./lab3.sh
+   80  nano lab3.sh
+   81  ./lab3.sh
+   82  nano lab3.sh
+   83  ./lab3.sh
+   84  ./lab3.sh 7
+   85  nano lab3.sh
+   86  ./lab3.sh 
+   87  nano lab3.sh
+   88  ./lab3.sh 
+   89  nano lab3.sh
+   90  ./lab3.sh 
+   91  nano lab3.sh
+   92  ./lab3.sh 7
+   93  nano lab3.sh
+   94  ./lab3.sh 
+   95  nano test.sh
+   96  gedit
+   97  vim
+   98  vi 
+   99   -r
+  100  -r
+  101  history
+  102  cd ..
+  103  week3
+  104  cd week3
+  105  ls
+  106  tar --help
+  107  history
+
