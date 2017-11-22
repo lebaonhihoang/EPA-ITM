e8f88f285e6b65105829735e90008bff2398f2a3
epa <epa@localhost.localdomain>
2017-11-22 14:11:17 +0000

    changes have been made to the loadWrapper.sh file

--- week8/loadWrapper.sh
+++ week8/loadWrapper.sh
@@ -1,6 +1,11 @@
-#!bin/bash
-./loadtest $1
+ #!/bin/bash
+# is to background the loadtest. 
+#process and detach it from the shell
+#fg will 
+./loadtest $1 & 
+
 sleep $2
+
 # kill the name of the process
 pkill loadtest
 
